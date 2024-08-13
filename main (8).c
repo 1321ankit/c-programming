306b7ea6d866d986bdb04eff92c7e60162f9131e
1321ankit <prajapatiankit1321@gmail.com>
2024-08-13 10:40:03 +0530

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,154 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+/*
+#include <stdio.h>
+
+int main()
+{
+  for(int i=1;i<=10;i++)
+  {
+      printf("%d \n",i);
+  }
+
+    return 0;
+}
+*/
+
+/*
+# include <stdio.h>
+int main()
+{
+    for(int i=50;i<=100;i++)
+    {
+        printf("%d \n",i);
+    }
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    for(int i=56;i<=78;i++)
+    {
+        printf("%d \n",i);
+
+    }
+}
+*/
+
+/*
+#include <stdio.h>
+int main()
+{
+    for(int i=10;i>=1;i--)
+    {
+    printf("%d \n",i);
+    }
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    for(int i=100;i>=50;i--)
+    {
+        printf("%d \n",i);
+    }
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    for(int i=78;i>=56;i--)
+    {
+        printf("%d \n",i);
+    }
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d \n",i);
+        }
+    }
+}
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+	for(int i=78; i>=56; i--)
+	{
+		if(i%2!=0)
+		{
+			printf("%d \n",i);
+		}
+	}
+	return 0;
+}
+*/
+
+
+/*
+#include<stdio.h>
+int main()
+{
+    int sum=0;
+    for(int i=1;i<=10;i++)
+    {
+         sum=sum+i;
+
+    }
+     printf("the sum of first 10 number is %d \n",sum);
+    return 0;
+}
+*/
+
+/*
+//factorial
+#include<stdio.h>
+int main()
+{
+    int fact=1;
+    for(int i=1;i<=5;i++)
+    {
+     fact=fact*i;   
+    }
+    printf("%d",fact);
+    return 0;
+}
+*/
+
+/*
+//fibonnaci series
+#include<stdio.h>
+int main()
+{
+    int n1=0,n2=1,n3;
+    printf("%d %d ",n1,n2);
+    for(int i=1;i<=5;i++)
+    {
+        n3=n1+n2;
+        printf("%d ",n3);
+        n1=n2;
+        n2=n3;
+    }
+    return 0;
+}
+*/
